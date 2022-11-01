5026e35675b40990da71953503d9d1a49f8371ed
SlinkovaOlga <sanch9713010@gmail.com>
2022-11-01 21:06:29 +0300

    First commit

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class CatTest {
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    private Feline feline;
+
+    @Test
+    public void catGetSoundReturnsCorrectValue(){
+        Cat cat = new Cat(feline);
+        String expected = "Мяу";
+        String actual = cat.getSound();
+
+        Assert.assertEquals("Expected value does not equal actual", expected, actual);
+    }
+
+    @Test
+    public void catGetFoodReturnsCorrectList() throws Exception {
+        Cat cat = new Cat(feline);
+        List<String> expected = List.of("Животные", "Птицы", "Рыба");
+        Mockito.when(feline.eatMeat()).thenReturn(expected);
+        List<String> actual = cat.getFood();
+
+        Assert.assertEquals("Expected value does not equal actual", expected, actual);
+    }
 }
